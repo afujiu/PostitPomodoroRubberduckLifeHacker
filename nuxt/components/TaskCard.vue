ce4689d2f8b7e5edb9075aea2478198384b88224
藤生明人 <akitof11@gmail.com>
2023-09-06 10:07:28 +0900

    修正

--- nuxt/components/TaskCard.vue
+++ nuxt/components/TaskCard.vue
@@ -6,31 +6,6 @@
       class="pa-0 ma-0 rounded-0"
       :class="{ 'active-card': stateDial }"
     >
-      <v-container class="pt-0">
-        <v-row>
-          <v-col cols="10">
-            <v-row>
-              <v-col cols="12" class="px-1 pt-0 pb-2">
-                <v-text-field
-                  v-model="task.title"
-                  class="my-0 py-0"
-                  dence
-                  hide-details
-                  :prepend-icon="isOpenOption ? 'mdi-menu-up' : 'mdi-menu-down'"
-                  @click:prepend="isOpenOption = !isOpenOption"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" class="ma-0 pa-0 px-2">
-                <span class="overline"> </span>
-              </v-col>
-              <v-col v-if="isOpenOption" cols="12" class="ma-0 pa-0 px-2">
-                おぷしょん
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="2" class="px-0 py-0" align="right"> </v-col>
-        </v-row>
-      </v-container>
     </v-card>
   </div>
 </template>
