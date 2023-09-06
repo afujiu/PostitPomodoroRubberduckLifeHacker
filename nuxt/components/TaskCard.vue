deb8d5d8ad731d55f98760b04d40b233401b94d0
藤生明人 <akitof11@gmail.com>
2023-09-06 10:17:21 +0900

    スピンを修正

--- nuxt/components/TaskCard.vue
+++ nuxt/components/TaskCard.vue
@@ -9,14 +9,47 @@
       <v-container class="pt-0">
         <v-row>
           <v-col cols="10">
-            <v-text-field
-              v-model="task.title"
-              class="my-0 py-0"
-              dence
-              hide-details
-              :prepend-icon="isOpenOption ? 'mdi-menu-up' : 'mdi-menu-down'"
-              @click:prepend="isOpenOption = !isOpenOption"
-            ></v-text-field>
+            <v-row>
+              <v-col cols="12" class="px-1 pt-0 pb-2">
+                <v-text-field
+                  v-model="task.title"
+                  class="my-0 py-0"
+                  dence
+                  hide-details
+                  :prepend-icon="isOpenOption ? 'mdi-menu-up' : 'mdi-menu-down'"
+                  @click:prepend="isOpenOption = !isOpenOption"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" class="ma-0 pa-0 px-2">
+                <span class="overline">
+                  <parts-date></parts-date>
+                </span>
+              </v-col>
+              <v-col v-if="isOpenOption" cols="12" class="ma-0 pa-0 px-2">
+                おぷしょん
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="2" class="px-0 py-0" align="right">
+            <v-speed-dial
+              v-model="stateDial"
+              direction="bottom"
+              style="width: 100%; height: 100%"
+              class="overline"
+            >
+              <template v-slot:activator>
+                <v-btn
+                  elevation="0"
+                  class="rounded-0 overline"
+                  :class="$db.task.stateList[task.state].textColor"
+                  :color="$db.task.stateList[task.state].color"
+                  style="width: 100%; height: 100%"
+                >
+                  {{ $db.task.stateList[task.state].text }}
+                </v-btn>
+              </template>
+              <span></span>
+            </v-speed-dial>
           </v-col>
         </v-row>
       </v-container>
