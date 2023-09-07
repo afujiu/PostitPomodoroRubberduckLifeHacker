ef8968bc3f32845c746787e109b9732c78d4d4c9
藤生明人 <akitof11@gmail.com>
2023-09-07 12:17:11 +0900

    タスク追加修正

--- nuxt/components/TaskCard.vue
+++ nuxt/components/TaskCard.vue
@@ -1,21 +1,39 @@
 <template>
   <div id="wrap">
-    <v-card variant="outlined" elevation="2" class="pa-0 ma-0 rounded-0" :class="{ 'active-card': stateDial }"
-      v-if="task != null">
+    <v-card
+      variant="outlined"
+      elevation="2"
+      class="pa-0 ma-0 rounded-0"
+      :class="{ 'active-card': isStateDial || isAddTaskDial }"
+      v-if="task != null"
+    >
       <v-container class="pt-0">
         <v-row>
           <v-col cols="10">
             <v-row>
               <v-col cols="1" class="pl-0 pr-2 pt-0 pb-0">
-                <v-btn fab x-small icon><v-icon small>mdi-plus</v-icon></v-btn>
+                <v-speed-dial v-model="isAddTaskDial" direction="bottom">
+                  <template v-slot:activator>
+                    <v-btn fab x-small icon
+                      ><v-icon small>mdi-plus</v-icon></v-btn
+                    >
+                  </template>
+                  <v-btn elevation="10" class="rounded-0 overline"
+                    ><v-icon>mdi-expand-all-outline</v-icon>小タスク</v-btn
+                  >
+                  <v-btn elevation="10" class="rounded-0 overline"
+                    ><v-icon>mdi-content-copy</v-icon>コピー</v-btn
+                  >
+                </v-speed-dial>
               </v-col>
               <v-col cols="11" class="px-1 pt-0 pb-0">
                 <v-text-field
                   v-model="task.title"
                   class="my-0 py-0"
-                  dence hide-details
-                  @change="$db.task.list=$db.task.list"
-                  >
+                  dence
+                  hide-details
+                  @change="$db.task.list = $db.task.list"
+                >
                 </v-text-field>
               </v-col>
               <v-col cols="12" class="ma-0 pa-0 px-2">
@@ -24,17 +42,35 @@
             </v-row>
           </v-col>
           <v-col cols="2" class="px-0 py-0" align="right">
-            <v-speed-dial v-model="stateDial" direction="bottom" style="width: 100%; height: 100%" class="overline">
+            <v-speed-dial
+              v-model="isStateDial"
+              direction="bottom"
+              style="width: 100%; height: 100%"
+              class="overline"
+            >
               <template v-slot:activator>
-                <v-btn elevation="0" class="rounded-0 overline" :class="$db.task.stateList[task.state].textColor"
-                  :color="$db.task.stateList[task.state].color" style="width: 100%; height: 100%">
+                <v-btn
+                  elevation="0"
+                  class="rounded-0 overline"
+                  :class="$db.task.stateList[task.state].textColor"
+                  :color="$db.task.stateList[task.state].color"
+                  style="width: 100%; height: 100%"
+                >
                   {{ $db.task.stateList[task.state].text }}
                 </v-btn>
               </template>
               <span></span>
-              <span v-for="(val, idx) in $db.task.stateList[task.state].next" :key="idx">
-                <v-btn elevation="10" class="rounded-0 overline" :class="$db.task.stateList[val].textColor"
-                  :color="$db.task.stateList[val].color" @click="changeState(val)">
+              <span
+                v-for="(val, idx) in $db.task.stateList[task.state].next"
+                :key="idx"
+              >
+                <v-btn
+                  elevation="10"
+                  class="rounded-0 overline"
+                  :class="$db.task.stateList[val].textColor"
+                  :color="$db.task.stateList[val].color"
+                  @click="changeState(val)"
+                >
                   {{ $db.task.stateList[val].text }}
                 </v-btn>
               </span>
@@ -54,11 +90,11 @@ export default {
   },
   data: () => ({
     task: null,
-    stateDial: false,
+    isStateDial: false,
+    isAddTaskDial: false,
     isOpenOption: false,
   }),
-  async created() {
-  },
+  async created() {},
   async mounted() {
     this.task = this.$db.task.getTask(this.id);
   },
@@ -74,9 +110,9 @@ export default {
 };
 </script>
 <style scoped>
-.v-text-field>>>input {
-  font-size:0.7em;
-  padding-bottom:0px;
+.v-text-field >>> input {
+  font-size: 0.7em;
+  padding-bottom: 0px;
 }
 
 .active-card {
