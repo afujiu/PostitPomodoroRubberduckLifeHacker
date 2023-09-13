f77be1251e1a4428bbb89fceb01b496dd6c31596
藤生明人 <akitof11@gmail.com>
2023-09-14 08:41:47 +0900

    セレクトボックス仮実装

--- nuxt/components/tabs/task.vue
+++ nuxt/components/tabs/task.vue
@@ -1,26 +1,47 @@
 <template>
   <div id="wrap">
     <v-container class="px-2 mx-0">
-      <v-row >
+      <v-row>
         <v-col cols="2" class="pb-5 mb-5" align="left">
-          <v-btn class="primary" dark fab small icon app fixed @click="pushAddTask()">
+          <v-btn
+            class="primary"
+            dark
+            fab
+            small
+            icon
+            app
+            fixed
+            @click="pushAddTask()"
+          >
             <v-icon>mdi-plus</v-icon>
           </v-btn>
         </v-col>
-        <v-col cols="8" class="pb-5 mb-5">
-        </v-col>
+        <v-col cols="8" class="pb-5 mb-5"> </v-col>
         <!-- 右オプション-->
         <v-col cols="2" class="pb-5 mb-5" :class="{ 'active-top': isOption }">
-          <v-speed-dial v-model="isOption" fixed direction="bottom" style="right:20px;">
+          <v-speed-dial
+            v-model="isOption"
+            fixed
+            direction="bottom"
+            style="right: 20px"
+          >
             <template v-slot:activator>
-              <v-btn class="grey darken-1" dark fab small right icon ><v-icon>mdi-dots-horizontal</v-icon></v-btn>
+              <v-btn class="grey darken-1" dark fab small right icon
+                ><v-icon>mdi-dots-horizontal</v-icon></v-btn
+              >
             </template>
             <!--フィルター-->
-            <v-btn fab x-small right @click="isFilterCard=true">
+            <v-btn fab x-small right @click="isFilterCard = true">
               <v-icon>mdi-filter-outline</v-icon>
             </v-btn>
             <!--全削除-->
-            <v-btn fab x-small right class="white--text black" @click="pushReset()"> 
+            <v-btn
+              fab
+              x-small
+              right
+              class="white--text black"
+              @click="pushReset()"
+            >
               <v-icon>mdi-trash-can</v-icon>
             </v-btn>
           </v-speed-dial>
@@ -37,21 +58,34 @@
           v-for="(val, idx) in $db.task.getParentList(null)"
           :key="idx"
         >
-          <task-card :id="val.id" elevation=12></task-card>
+          <task-card :id="val.id" elevation="12"></task-card>
         </v-col>
       </v-row>
       <v-row>
-      <v-card v-if="isFilterCard" class="filter-card">
-        <v-container>
-          <v-row>
-            <v-col cols="12">a</v-col>
-            <v-col cols="12">b</v-col>
-            <v-col cols="12">c</v-col>
-            <v-col cols="12">d</v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-row>
+        <v-card v-if="isFilterCard" class="filter-card">
+          <v-container>
+            <v-row>
+              <v-col cols="12"> </v-col>
+              <v-col cols="12">
+                <v-select
+                  label="Select"
+                  :items="[
+                    'California',
+                    'Colorado',
+                    'Florida',
+                    'Georgia',
+                    'Texas',
+                    'Wyoming',
+                  ]"
+                  multiple
+                ></v-select>
+              </v-col>
+              <v-col cols="12">c</v-col>
+              <v-col cols="12">d</v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -60,7 +94,7 @@ export default {
   data: () => ({
     isOption: false,
     isForceOption: true,
-    isFilterCard:false,
+    isFilterCard: false,
   }),
   created() {},
   mounted() {
@@ -85,11 +119,11 @@ export default {
 };
 </script>
 <style scoped>
-.filter-card{
-  position:fixed;
-  margin:1em;
-  width:95%;
-  bottom:0;
-  z-index:999;
+.filter-card {
+  position: fixed;
+  margin: 1em;
+  width: 95%;
+  bottom: 0;
+  z-index: 999;
 }
 </style>
