7b3f149b5016d81793c546f794d7f15bb1178213
藤生明人 <akitof11@gmail.com>
2023-09-21 08:34:05 +0900

    総作業時間追加

--- nuxt/components/tabs/result.vue
+++ nuxt/components/tabs/result.vue
@@ -24,7 +24,11 @@
         <v-col cols="12">
           <p class="h6">今日の作業</p>
         </v-col>
-        <v-col class="my-0 mt-4" xm="12" sm="12" md="12" lg="12" cols="12">
+        <v-col cols="12">
+          総作業時間
+          <p>{{ $plg.util.msToHour(sumTime, false) }}:{{ sumTime }}</p>
+        </v-col>
+        <v-col class="my-0 mt-0" xm="12" sm="12" md="12" lg="12" cols="12">
           <div v-for="(val, uid) in list" :key="uid">
             <span v-if="$plg.task.getTask(uid) != null">
               <span>
@@ -43,8 +47,8 @@
                 >
               </span>
               <span v-if="isWrokTime">
-                {{ $plg.util.msToMinute(val.sumWorkTime, false) }}:{{
-                  $plg.util.msToSecond(val.sumWorkTime)
+                {{ $plg.util.msToHour(val.sumWorkTime, false) }}:{{
+                  $plg.util.msToMinute(val.sumWorkTime)
                 }}
               </span>
             </span>
@@ -58,10 +62,15 @@
 export default {
   data: () => ({
     list: [],
+    sumTime: 0,
     isWrokTime: true,
   }),
   async created() {
     this.list = await this.$plg.log.getToday();
+    this.sumTime = 0;
+    for (let val of this.list) {
+      this.sumTime += val.sumWorkTime;
+    }
   },
   async mounted() {
     this.$plg.task.initLoadFunction(() => {
